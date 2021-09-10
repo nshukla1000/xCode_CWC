1d98c9c2bbb9bebccbdcc972fbb238016e89de5f
Nilesh Shukla <nileshshukla@gmail.com>
2021-09-09 22:23:57 -0500

    Initial Commit

---
+++ M2C16_Quotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M2C16_Quotes
+//
+//  Created by Tarjani Shukla on 9/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
